434b0b48f27e7e63536301022d93517aa1ef4f70
hotta-ryohei <hotta.ryohei@gmail.com>
2023-09-25 14:04:00 +0900

    post

--- database/migrations/2023_09_11_053402_create_posts_table.php
+++ database/migrations/2023_09_11_053402_create_posts_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->unsignedBigInteger('id');
             $table->unsignedBigInteger('user_id');
-            $table->varchar(50)('name');
+            $table->string('name');
             $table->text('contents');
         });
     }
